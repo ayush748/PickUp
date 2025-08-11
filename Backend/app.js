192a48f971afc257d1f46737e3f326d5d3a431f5
Ayush Tiwari <iamayush522@gmail.com>
2025-08-11 23:19:50 +0530

    serverchange

--- Backend/app.js
+++ Backend/app.js
@@ -1,29 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
-// they are used to load environment variables from a .env file into process.env
-const express = require('express');
-const app = express();
-const cors =require('cors');
-const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes');
+const express =require('express');
+const cors = require('cors');
+const app =express();
+
 
 app.use(cors());
-// this is for enabling CORS, which allows your server to accept requests from different origins
-app.use(express.json());
-// this is for parsing incoming JSON requests and putting the parsed data in req.body
-app.use(express.urlencoded({ extended: true }));
-
-
-
 
 
 app.get('/',(req,res)=>{
     res.send('Hello World')
-})
+});
 
- 
-
-app.use('/users', userRoutes);
-
-// this is for exporting the app instance
-module.exports = app;
+module.exports =app;
